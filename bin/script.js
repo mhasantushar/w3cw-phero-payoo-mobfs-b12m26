67a7f24c91fa80b4ea28b7967913d8873834f796
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-08-23 13:30:21 +0600

    login form scrpit done

--- bin/script.js
+++ bin/script.js
@@ -1,1 +1,21 @@
-console.log ("yesp")
+const custMobile = "00123456789";
+const custPinCode = "1234";
+
+function isLoginGranted() {
+  const txtMobile = document.getElementById("fld-mobile").value;
+  const txtSecpin = document.getElementById("fld-secpin").value;
+
+  if (txtMobile === null || txtSecpin === null) return false;
+  if (txtMobile !== custMobile || txtSecpin !== custPinCode) return false;
+
+  return true;
+}
+
+const btnLogin = document.getElementById("btn-login");
+if (btnLogin) {
+  btnLogin.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (isLoginGranted()) console.log("ya");
+    else alert("Login attempt failed");
+  });
+}
