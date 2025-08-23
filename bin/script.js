b8dd402a92d5499dc39520f3b85bd3f607a35677
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-08-23 19:06:57 +0600

    main form act pane script in progress

--- bin/script.js
+++ bin/script.js
@@ -1,7 +1,7 @@
+//SECTION - LOGIN PAGE SCRIPTS STARTES HERE
 const custMobile = "00123456789";
 const custSecpin = "1234";
 
-//SECTION - LOGIN SCRIPTS
 function isLoginGranted() {
   const txtMobile = document.getElementById("fld-mobile").value;
   const txtSecpin = document.getElementById("fld-secpin").value;
@@ -22,10 +22,66 @@ if (btnLogin) {
     else alert("Login attempt failed");
   });
 }
-//!SECTION login scripts
+//!SECTION login scripts ends here
 
 
-//SECTION - NAVIGATION PANEL SCRIPTS
+//SECTION - MAIN PAGE SCRIPTS STARTS HERE
+let oldSelButton = "addmoney";
 
 
-//!SECTION - navigation panel scripts
+function invokeSelAction(newSelButtonId){
+
+
+
+  switch (selact) {
+
+  }
+}
+
+const btnAddmoney = document.getElementById("inv-addmoney");
+if (btnAddmoney){
+  btnAddmoney.addEventListener("click", function(e) {
+    e.preventDefault;
+    console.log("inv-addmoney");
+    })
+}
+const btnCashout = document.getElementById("inv-cashout");
+if (btnCashout){
+  btnCashout.addEventListener("click", function(e) {
+    e.preventDefault;
+    console.log("inv-cashout");
+    })
+}
+const btnTransfer = document.getElementById("inv-transfer");
+if (btnTransfer){
+  btnTransfer.addEventListener("click", function(e) {
+    e.preventDefault;
+    console.log("inv-transfer");
+    })
+}
+const btnGetbonus = document.getElementById("inv-getbonus");
+if (btnGetbonus){
+  btnGetbonus.addEventListener("click", function(e) {
+    e.preventDefault;
+    console.log("inv-getbonus");
+    })
+}
+const btnPaybill = document.getElementById("inv-paybill");
+if (btnPaybill){
+  btnPaybill.addEventListener("click", function(e) {
+    e.preventDefault;
+    console.log("inv-paybill");
+    })
+}
+const btnTransacts = document.getElementById("inv-transact");
+if (btnTransacts){
+  btnTransacts.addEventListener("click", function(e) {
+    e.preventDefault;
+    console.log("inv-transact");
+    })
+}
+
+
+
+
+//!SECTION - main page scripts ends here
