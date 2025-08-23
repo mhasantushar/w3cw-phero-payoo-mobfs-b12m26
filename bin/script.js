1e54c2d186d94d46f9eaec1f233a22384c202611
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-08-23 18:06:15 +0600

    main form act pane html half-done

--- bin/script.js
+++ bin/script.js
@@ -1,12 +1,15 @@
 const custMobile = "00123456789";
-const custPinCode = "1234";
+const custSecpin = "1234";
 
+//SECTION - LOGIN SCRIPTS
 function isLoginGranted() {
   const txtMobile = document.getElementById("fld-mobile").value;
   const txtSecpin = document.getElementById("fld-secpin").value;
 
   if (txtMobile === null || txtSecpin === null) return false;
-  if (txtMobile !== custMobile || txtSecpin !== custPinCode) return false;
+  if (txtMobile !== custMobile || txtSecpin !== custSecpin) return false;
+
+  console.log (txtMobile, txtSecpin)
 
   return true;
 }
@@ -15,7 +18,14 @@ const btnLogin = document.getElementById("btn-login");
 if (btnLogin) {
   btnLogin.addEventListener("click", function (e) {
     e.preventDefault();
-    if (isLoginGranted()) console.log("ya");
+    if (isLoginGranted()) window.location.href = "./main.html";
     else alert("Login attempt failed");
   });
 }
+//!SECTION login scripts
+
+
+//SECTION - NAVIGATION PANEL SCRIPTS
+
+
+//!SECTION - navigation panel scripts
