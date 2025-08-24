86c7443968482afbd57ba02218d3d24ebaa98d3b
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-08-24 18:54:06 +0600

    main form add money script done

--- bin/script.js
+++ bin/script.js
@@ -1,9 +1,13 @@
 const custMobile = "00123456789";
 const custSecpin = "1234";
 
-let oldSelButtonId = "nav-addmoney"; //the page loads with this button selected
+let oldSelNavigId = "nav-addmoney"; //the page loads with this navigation option selected
 
-//SECTION - LOGIN PAGE SCRIPTS STARTS HERE
+//SECTION - COMMON FUNCTIONS START HERE
+
+//!SECTION common functions end
+
+//SECTION - LOGIN PAGE SCRIPTS START HERE
 const btnLogin = document.getElementById("btn-login");
 if (btnLogin) {
   btnLogin.addEventListener("click", function (e) {
@@ -15,7 +19,7 @@ if (btnLogin) {
 
 function isLoginGranted() {
   const txtMobile = document.getElementById("fld-mobile").value;
-  const txtSecpin = document.getElementById("fld-secpin").value;
+  const txtSecpin = document.getElementById("fld-securpin-of-addm").value;
 
   if (txtMobile === null || txtSecpin === null) return false;
   if (txtMobile !== custMobile || txtSecpin !== custSecpin) return false;
@@ -24,9 +28,9 @@ function isLoginGranted() {
 
   return true;
 }
-//!SECTION login page scripts ends
+//!SECTION login page scripts end
 
-//SECTION - MAIN PAGE > COMMON NAVIG SECTION'S SCRIPTS STARTS HERE
+//SECTION - MAIN PAGE > COMMON NAVIG AREA SCRIPTS START HERE
 const navOptions = [
   "nav-addmoney",
   "nav-cashout",
@@ -41,7 +45,7 @@ for (const selOpt of navOptions) {
   if (navOpt) {
     navOpt.addEventListener("click", function (e) {
       e.preventDefault();
-      if (oldSelButtonId !== selOpt) processNewSelection(selOpt);
+      if (oldSelNavigId !== selOpt) processNewSelection(selOpt);
     });
   }
 }
@@ -52,76 +56,75 @@ for (const selOpt of navOptions) {
 // if (cmdAddmoney) {
 //   cmdAddmoney.addEventListener("click", function (e) {
 //     e.preventDefault();
-//     if (oldSelButtonId !== "nav-addmoney") processNewSelection("nav-addmoney");
+//     if (oldSelNavigId !== "nav-addmoney") processNewSelection("nav-addmoney");
 //   });
 // }
 // const cmdCashout = document.getElementById("nav-cashout");
 // if (cmdCashout) {
 //   cmdCashout.addEventListener("click", function (e) {
 //     e.preventDefault();
-//     if (oldSelButtonId !== "nav-cashout") processNewSelection("nav-cashout");
+//     if (oldSelNavigId !== "nav-cashout") processNewSelection("nav-cashout");
 //   });
 // }
 // const cmdTransfer = document.getElementById("nav-transfer");
 // if (cmdTransfer) {
 //   cmdTransfer.addEventListener("click", function (e) {
 //     e.preventDefault();
-//     if (oldSelButtonId !== "nav-transfer") processNewSelection("nav-transfer");
+//     if (oldSelNavigId !== "nav-transfer") processNewSelection("nav-transfer");
 //   });
 // }
 // const cmdGetbonus = document.getElementById("nav-getbonus");
 // if (cmdGetbonus) {
 //   cmdGetbonus.addEventListener("click", function (e) {
 //     e.preventDefault();
-//     if (oldSelButtonId !== "nav-getbonus") processNewSelection("nav-getbonus");
+//     if (oldSelNavigId !== "nav-getbonus") processNewSelection("nav-getbonus");
 //   });
 // }
 // const cmdPaybill = document.getElementById("nav-paybill");
 // if (cmdPaybill) {
 //   cmdPaybill.addEventListener("click", function (e) {
 //     e.preventDefault();
-//     if (oldSelButtonId !== "nav-paybill") processNewSelection("nav-paybill");
+//     if (oldSelNavigId !== "nav-paybill") processNewSelection("nav-paybill");
 //   });
 // }
 // const cmdTransact = document.getElementById("nav-transact");
 // if (cmdTransact) {
 //   cmdTransact.addEventListener("click", function (e) {
 //     e.preventDefault();
-//     if (oldSelButtonId !== "nav-transact") processNewSelection("nav-transact");
+//     if (oldSelNavigId !== "nav-transact") processNewSelection("nav-transact");
 //   });
 // }
 
-function processNewSelection(newSelButtonId) {
-  const oldSelButton = document.getElementById(oldSelButtonId);
-  const newSelButton = document.getElementById(newSelButtonId);
-  // console.log(oldSelButton.innerText, newSelButton.innerText);
+function processNewSelection(newSelNavigId) {
+  const oldSelNavig = document.getElementById(oldSelNavigId);
+  const newSelNavig = document.getElementById(newSelNavigId);
+  // console.log(oldSelNavig.innerText, newSelNavig.innerText);
 
-  const oldButtonLblId = "lbl" + oldSelButtonId.slice(3);
-  const newButtonLblId = "lbl" + newSelButtonId.slice(3);
-  const oldButtonLabel = document.getElementById(oldButtonLblId);
-  const newButtonLabel = document.getElementById(newButtonLblId);
-  // console.log(oldButtonLabel.innerText, newButtonLabel.innerText);
+  const oldNavigLblId = "lbl" + oldSelNavigId.slice(3);
+  const newNavigLblId = "lbl" + newSelNavigId.slice(3);
+  const oldNavigLabel = document.getElementById(oldNavigLblId);
+  const newNavigLabel = document.getElementById(newNavigLblId);
+  // console.log(oldNavigLabel.innerText, newNavigLabel.innerText);
 
-  //making new sel button blue themed and old sel button normai-themed...
-  oldSelButton.classList.remove("border-blue-500");
-  oldSelButton.classList.remove("bg-blue-50");
-  oldSelButton.classList.add("border-[#0874f20d]");
+  //making new sel navigation option blue themed and old sel navigation option normai-themed...
+  oldSelNavig.classList.remove("border-blue-500");
+  oldSelNavig.classList.remove("bg-blue-50");
+  oldSelNavig.classList.add("border-[#0874f20d]");
+  newSelNavig.classList.remove("border-[#0874f20d]");
+  newSelNavig.classList.add("border-blue-500");
+  newSelNavig.classList.add("bg-blue-50");
 
-  newSelButton.classList.remove("border-[#0874f20d]");
-  newSelButton.classList.add("border-blue-500");
-  newSelButton.classList.add("bg-blue-50");
-
-  //doing the same treatment for new and old buttons's labels...
-  oldButtonLabel.classList.remove("font-bold");
-  oldButtonLabel.classList.remove("text-blue-500");
-  oldButtonLabel.classList.add("text-[#080808b3]");
-  newButtonLabel.classList.remove("text-[#080808b3]");
-  newButtonLabel.classList.add("font-bold");
-  newButtonLabel.classList.add("text-blue-500");
+  //doing the same treatment for new and old navigation option's labels...
+  oldNavigLabel.classList.remove("font-bold");
+  oldNavigLabel.classList.remove("text-blue-500");
+  oldNavigLabel.classList.add("text-[#080808b3]");
+  newNavigLabel.classList.remove("text-[#080808b3]");
+  newNavigLabel.classList.add("font-bold");
+  newNavigLabel.classList.add("text-blue-500");
 
   //now, invoking associated action panel...
   //TODO - need to code to show/hide bottom panels based on user selection
-  switch (newSelButtonId) {
+  switch (newSelNavigId) {
     case "nav-addmoney": {
       break;
     }
@@ -142,48 +145,78 @@ function processNewSelection(newSelButtonId) {
     }
   }
 
-  oldSelButtonId = newSelButtonId;
+  oldSelNavigId = newSelNavigId;
 }
-//!SECTION - main page > common navig section's scripts ends
+//!SECTION - main page > common navig area scripts end
 
-//SECTION - BOTTOM TABBED AREA > ADD MONEY STARTS...
+//SECTION - BOTTOM TABBED > TAB ADD MONEY SCRIPTS START HERE
 const btnAddmoney = document.getElementById("btn-addmoney");
 if (btnAddmoney) {
   btnAddmoney.addEventListener("click", function (e) {
     e.preventDefault();
-    processAddmoneyReq() ? alert("Add money succeeded!") : alert("Add money failed!");
+    if (processAddmoneyReq()) alert("Add money succeeded!");
   });
 }
 
 function processAddmoneyReq() {
-  let element, uInput;
+  let element;
+  let uInput;
 
-  element = document.getElementById("fld-custbank");
-  if (!element) return false;
+  element = document.getElementById("fld-custbank-of-addm");
+  console.log(element);
+  if (!element) {
+    alert("Bank field not found");
+    return false;
+  }
   uInput = element.value.replaceAll(" ", "");
-  if (uInput.length === 0) return false;
-
-  element = document.getElementById("fld-custbacc");
-  if (!element) return false;
+  console.log(uInput);
+  if (uInput.length === 0) {
+    alert("Bank name not specified");
+    return false;
+  }
+  element = document.getElementById("fld-custbacc-of-addm");
+  console.log(element);
+  if (!element) {
+    alert("Account field not found");
+    return false;
+  }
   uInput = element.value.replaceAll(" ", "");
-  if (uInput.length !== 11) return false;
-
-  element = document.getElementById("fld-secpin");
-  if (!element) return false;
+  console.log(uInput);
+  if (uInput.length !== 11) {
+    alert("Account value not specified or invaid");
+    return false;
+  }
+  element = document.getElementById("fld-securpin-of-addm");
+  console.log(element);
+  if (!element) {
+    alert("Pin field not found");
+    return false;
+  }
   uInput = element.value.replaceAll(" ", "");
-  if (uInput !== custSecpin) return false;
+  console.log(uInput);
+  if (uInput !== custSecpin) {
+    alert("Pin value not specified or invalid");
+    return false;
+  }
 
-  element = document.getElementById("fld-addamount");
-  if (!element) return false;
+  element = document.getElementById("fld-moneyval-of-addm");
+  console.log(element);
+  if (!element) {
+    alert("Amount field not found");
+    return false;
+  }
   uInput = parseFloat(element.value.replaceAll(" ", ""));
-  if (uInput <= 0 || Number.isNaN(uInput)) return false;
+  console.log(uInput);
+  if (uInput <= 0 || Number.isNaN(uInput)) {
+    alert("Amount value not specified or invalid");
+    return false;
+  }
 
-  const custCurrBal = document.getElementById("currBalance").innerText;
-  uInput = uInput + parseFloat(custCurrBal);
-  document.getElementById("currBalance").innerText = uInput.toFixed(2);
-  document.getElementById("fld-addamount").value = null;
+  element = document.getElementById("lbl-cust-currbalance");
+  const newBalance = uInput + parseFloat(element.innerText);
+  element.innerText = newBalance.toFixed(2);
+  document.getElementById("fld-moneyval-of-addm").value = "";
 
   return true;
 }
-
-//!SECTION - add money area ends
+//!SECTION - tab add money scripts end
